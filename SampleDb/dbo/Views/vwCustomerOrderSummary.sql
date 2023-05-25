263b5019939edc9dbaa81f1abbe7fe8754076799
Emmanuel Twum Osafo <72449645+EmmanuelTwum@users.noreply.github.com>
2023-05-25 11:02:19 +0000

    Add project files.

---
+++ SampleDb/dbo/Views/vwCustomerOrderSummary.sql
@@ -0,0 +1,19 @@
+﻿
+--create a handy view summarizing customer orders
+CREATE VIEW vwCustomerOrderSummary WITH SCHEMABINDING AS
+	SELECT
+	c.CustomerId
+,	c.FirstName
+,	c.LastName
+--,	c.CustomerRanking
+,	ISNULL(SUM(oh.OrderTotal),0) AS OrderTotal
+
+	FROM dbo.Customer AS c
+	LEFT OUTER JOIN dbo.OrderHeader AS oh 
+	ON c.CustomerId = oh.CustomerId
+
+	GROUP BY 
+	c.CustomerId
+,	c.FirstName
+,	c.LastName
+--,	c.CustomerRanking
